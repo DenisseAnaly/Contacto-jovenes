c00ba42ce879241ffccfa9b1f331fddf83d4e6c8
DenisseAnaly <80987723+DenisseAnaly@users.noreply.github.com>
2021-03-19 15:46:43 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/cabecera.css">
+</head>
+<body >
+    <form action="validar.php" method="post">
+    <h1>Iniciar Sesión</h1>
+    <p>Usuario <input type="text" placeholder="Ingrese su nombre" name="usuario"></p>
+    <p>Contraseña <input type="password" placeholder="Ingrese su contraseña" name="contraseña"></p>
+    <input type="submit" value="Ingresar">
+    </form>
+</body>
+</html>
